c07b96e9a299ca9368616255c208bbfbd4d569e9
mposadas1394 <matiasposadas1394@gmail.com>
2023-06-12 00:07:53 -0300

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Random randomGenerator = new Random();
+       int number = randomGenerator.Next(1, 100);
+       int answer = -1;
+       
+       while(answer!=number){
+        Console.WriteLine("what´s the number?: ");
+        string userInput=Console.ReadLine();
+        answer=int.Parse(userInput);
+
+        if(answer>number){
+            Console.WriteLine("Lower");
+        
+            }
+        else if(answer<number){
+            Console.WriteLine("Higher");
+        }
+        else{
+            Console.WriteLine("You guessed it");
+        }
+       }
+       
     }
 }
